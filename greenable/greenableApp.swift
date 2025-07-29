24058a9c613f11537fd4ca033d2f146605260a4a
Anushree <anushreers2009@gmail.com>
2025-07-29 13:40:57 -0500

    Initial Commit

---
+++ greenable/greenableApp.swift
@@ -0,0 +1,17 @@
+//
+//  greenableApp.swift
+//  greenable
+//
+//  Created by Anushree RS on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct greenableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
