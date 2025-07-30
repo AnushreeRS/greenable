10f1472f3a9394c24cae611661fa0b075758c8a8
Olive Luong <olivee.yan@gmail.com>
2025-07-29 18:59:21 -0700

    made the survey view 2

--- greenable/survey.swift
+++ greenable/survey.swift
@@ -9,7 +9,167 @@ import SwiftUI
 
 struct survey: View {
     var body: some View {
-        Text("Survey")
+        ZStack {
+            Color(red:0.85, green:0.95, blue: 0.81)
+                .ignoresSafeArea()
+            ScrollView {
+                VStack (spacing: 10.0){
+                    Text("Greenable")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.255, green: 0.53, blue: 0.35))
+                    Text("TELL US ABOUT YOUR GREEN LEVEL")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.brown)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 18)
+                    Text("1. On a scale of 1-4, how environmentally friendly are you? (1 is not at all and 4 is super-duper)")
+                        .multilineTextAlignment(.leading)
+                    HStack (spacing: 40.0) {
+                        Button {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        } label: {
+                            Text ("1")
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 9)
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                                .cornerRadius(50)
+                        }
+                        Button {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        } label: {
+                            Text ("2")
+                                .padding(.horizontal, 17.5)
+                                .padding(.vertical, 9)
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                                .cornerRadius(50)
+                        }
+                        Button {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        } label: {
+                            Text ("3")
+                                .padding(.horizontal, 17)
+                                .padding(.vertical, 9)
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                                .cornerRadius(50)
+                        }
+                        Button {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        } label: {
+                            Text ("4")
+                                .padding(.horizontal, 17)
+                                .padding(.vertical, 9)
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                                .cornerRadius(50)
+                        }
+                    }
+                    Text("2. Which task do you think would be the easiest for you to complete?")
+                        .multilineTextAlignment(.leading)
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Where ever you go today, pick up 1+ piece of litter you see")
+                            .padding(.horizontal, 32)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                    }
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Where ever you go today, pick up 5+ pieces of litter you see")
+                            .padding(.horizontal, 32)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                }
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("In addition to where you're going today, walk or bike around your area and pick up 15+ pieces of litter")
+                            .padding(.horizontal, 24)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                }
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Spend the day out in your area picking up all the litter you see! Then, bring them to your local compound for some cash")
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                }
+                    Text("3. Which task do you think would be the hardest for you to complete?")
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Be mindful of your water usage today!")
+                            .padding(.horizontal, 30)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                    }
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Use less water than you did yesterday")
+                            .padding(.horizontal, 30)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                }
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Don't let water run for more than a total of 15 minutes all day!")
+                            .padding(.horizontal, 25)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                }
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Don't let water run for more than a total of 10 minutes all day!")
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.255, green: 0.53, blue: 0.35))
+                            .cornerRadius(40)
+                }
+                    Button {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    } label: {
+                        Text ("Submit")
+                            .font(.title)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .foregroundColor(Color.white)
+                            .background(Color(red: 0.55, green: 0.42, blue: 0.32))
+                            .cornerRadius(40)
+                }
+                }
+                .padding(22)
+            }
+        }
     }
 }
 
