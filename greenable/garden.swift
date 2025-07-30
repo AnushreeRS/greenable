1d8119886bddfd90e5dcb14fc3db0fead21e1ac5
Sophie <sunfloweryam@gmail.com>
2025-07-30 10:26:58 -0700

    added a title and background

--- greenable/garden.swift
+++ greenable/garden.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct garden: View {
     var body: some View {
-        
-        Text("Garden")
-        Text ("Hi")
+        ZStack {
+            Color(red: 213/255, green: 255/255, blue: 255/255)
+                .ignoresSafeArea(edges: .all)
+            VStack {
+                Text("Garden")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.435, saturation: 0.888, brightness: 0.36))
+                Spacer()
+            }
+          
+        }
     }
+    
+    
 }
-
 #Preview {
     garden()
 }
