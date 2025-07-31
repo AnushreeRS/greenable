59518ab38bb5bd68a41950c3ede59a8470063b9b
Olive Luong <olivee.yan@gmail.com>
2025-07-30 19:10:59 -0700

    made it more pretty

--- greenable/survey.swift
+++ greenable/survey.swift
@@ -10,21 +10,20 @@ import SwiftUI
 struct survey: View {
     var body: some View {
         ZStack {
-            Color(red:0.85, green:0.95, blue: 0.81)
+            Color(red:0.97, green:0.96, blue: 0.92)
                 .ignoresSafeArea()
             ScrollView {
                 VStack (spacing: 10.0){
                     Text("Greenable")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
+                        .font(.custom("LibreBaskerville-Bold", size: 55))
                         .foregroundColor(Color(red: 0.255, green: 0.53, blue: 0.35))
                     Text("TELL US ABOUT YOUR GREEN LEVEL")
-                        .font(.title3)
-                        .fontWeight(.semibold)
+                        .font(.custom("Poppins", size: 20))
                         .foregroundColor(Color.brown)
                         .multilineTextAlignment(.center)
                         .padding(.horizontal, 18)
                     Text("1. On a scale of 1-4, how environmentally friendly are you? (1 is not at all and 4 is super-duper)")
+                        .font(.custom("Source Sans Pro", size: 18))
                         .multilineTextAlignment(.leading)
                     HStack (spacing: 40.0) {
                         Button {
@@ -33,7 +32,7 @@ struct survey: View {
                             Text ("1")
                                 .padding(.horizontal, 20)
                                 .padding(.vertical, 9)
-                                .font(.title)
+                                .font(.custom("Source Sans Pro", size: 29))
                                 .foregroundColor(Color.white)
                                 .background(Color(red: 0.255, green: 0.53, blue: 0.35))
                                 .cornerRadius(50)
@@ -42,9 +41,9 @@ struct survey: View {
                             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                         } label: {
                             Text ("2")
-                                .padding(.horizontal, 17.5)
+                                .padding(.horizontal, 20)
                                 .padding(.vertical, 9)
-                                .font(.title)
+                                .font(.custom("Source Sans Pro", size: 29))
                                 .foregroundColor(Color.white)
                                 .background(Color(red: 0.255, green: 0.53, blue: 0.35))
                                 .cornerRadius(50)
@@ -53,9 +52,9 @@ struct survey: View {
                             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                         } label: {
                             Text ("3")
-                                .padding(.horizontal, 17)
+                                .padding(.horizontal, 20)
                                 .padding(.vertical, 9)
-                                .font(.title)
+                                .font(.custom("Source Sans Pro", size: 29))
                                 .foregroundColor(Color.white)
                                 .background(Color(red: 0.255, green: 0.53, blue: 0.35))
                                 .cornerRadius(50)
@@ -64,21 +63,23 @@ struct survey: View {
                             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                         } label: {
                             Text ("4")
-                                .padding(.horizontal, 17)
+                                .padding(.horizontal, 20)
                                 .padding(.vertical, 9)
-                                .font(.title)
+                                .font(.custom("Source Sans Pro", size: 29))
                                 .foregroundColor(Color.white)
                                 .background(Color(red: 0.255, green: 0.53, blue: 0.35))
                                 .cornerRadius(50)
                         }
                     }
                     Text("2. Which task do you think would be the easiest for you to complete?")
+                        .font(.custom("Source Sans Pro", size: 18))
                         .multilineTextAlignment(.leading)
                     Button {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Where ever you go today, pick up 1+ piece of litter you see")
-                            .padding(.horizontal, 32)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 23)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
@@ -88,7 +89,8 @@ struct survey: View {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Where ever you go today, pick up 5+ pieces of litter you see")
-                            .padding(.horizontal, 32)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 21)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
@@ -98,7 +100,8 @@ struct survey: View {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("In addition to where you're going today, walk or bike around your area and pick up 15+ pieces of litter")
-                            .padding(.horizontal, 24)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 23)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
@@ -108,18 +111,21 @@ struct survey: View {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Spend the day out in your area picking up all the litter you see! Then, bring them to your local compound for some cash")
-                            .padding(.horizontal, 15)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 14)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
                             .cornerRadius(40)
                 }
                     Text("3. Which task do you think would be the hardest for you to complete?")
+                        .font(.custom("Source Sans Pro", size: 18))
                     Button {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Be mindful of your water usage today!")
-                            .padding(.horizontal, 30)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 38)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
@@ -129,7 +135,8 @@ struct survey: View {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Use less water than you did yesterday")
-                            .padding(.horizontal, 30)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 40)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
@@ -139,7 +146,8 @@ struct survey: View {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Don't let water run for more than a total of 15 minutes all day!")
-                            .padding(.horizontal, 25)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 23)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
@@ -149,7 +157,8 @@ struct survey: View {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Don't let water run for more than a total of 10 minutes all day!")
-                            .padding(.horizontal, 15)
+                            .font(.custom("Source Sans Pro", size: 17))
+                            .padding(.horizontal, 13)
                             .padding(.vertical, 5)
                             .foregroundColor(Color.white)
                             .background(Color(red: 0.255, green: 0.53, blue: 0.35))
@@ -159,6 +168,7 @@ struct survey: View {
                         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     } label: {
                         Text ("Submit")
+                            .font(.custom("Source Sans Pro", size: 25))
                             .font(.title)
                             .padding(.horizontal, 15)
                             .padding(.vertical, 5)
@@ -166,6 +176,7 @@ struct survey: View {
                             .background(Color(red: 0.55, green: 0.42, blue: 0.32))
                             .cornerRadius(40)
                 }
+                    .padding(20)
                 }
                 .padding(22)
             }
