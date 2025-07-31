454502665a7bda7869e05479ca4c1d6b417153cc
Sophie <sunfloweryam@gmail.com>
2025-07-31 11:49:32 -0700

    changed color of background

--- greenable/garden.swift
+++ greenable/garden.swift
@@ -9,8 +9,10 @@
 import SwiftUI
 
 struct garden: View {
+    @State private var currentImageIndex: Int = 0
+    let imageNames: [String] = ["blueseed", "bluesprout", "bluebud", "blueflower"]
     var body: some View {
-        ZStack{ Color(.white)
+        ZStack{ Color (red: 0.969, green: 0.961, blue: 0.92)
                 .ignoresSafeArea()
             VStack {
                 Spacer()
@@ -23,12 +25,14 @@ struct garden: View {
                         .foregroundColor(Color(hue: 0.435, saturation: 0.888, brightness: 0.36))
                     Spacer()
                     Text(" 🍁__leafpoints")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hue: 0.435, saturation: 0.888, brightness: 0.36))
                         .padding()
                 }
                 .padding()
                 Spacer()
                 
-                Color(.white) //red: 213/255, green: 255/255, blue: 255/255)
+                Color(red: 213/255, green: 255/255, blue: 255/255)
                     .padding()
                     .background(Rectangle())
                     .foregroundStyle(Color(red: 131/255, green: 105/255, blue: 83/255))
@@ -38,12 +42,26 @@ struct garden: View {
             }
             
             VStack{
-             
+           
         
-                Image("seed")
-                    .resizable()
-                    .frame(width: 320, height: 500)
-                    .padding()
+                if currentImageIndex == 0 {
+                              Image("blueseed")
+                                  .resizable()
+                                  .scaledToFit()
+                                  .frame(width: 200, height: 150)
+                          } else if currentImageIndex == 1 {
+                              Image("bluesprout")
+                                  .resizable()
+                                  .scaledToFit()
+                          } else if currentImageIndex == 2 {
+                              Image("bluebud")
+                                  .resizable()
+                                  .scaledToFit()
+                          } else { // currentImageIndex == 3 or any other value
+                              Image("blueflower")
+                                  .resizable()
+                                  .scaledToFit()
+                          }
                     
                 
             }
