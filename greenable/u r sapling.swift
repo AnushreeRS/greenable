516b5ac08219b76fb06bb7cdee3cba573ec70862
Olive Luong <olivee.yan@gmail.com>
2025-07-31 13:57:40 -0700

    navigation stack on u r sapling to garden

--- greenable/u r sapling.swift
+++ greenable/u r sapling.swift
@@ -9,19 +9,22 @@ import SwiftUI
 
 struct u_r_sapling: View {
     var body: some View {
-        ZStack {
+        NavigationStack {
+            ZStack {
                 Color(red:0.97, green:0.96, blue: 0.92)
                     .ignoresSafeArea()
-            VStack {
-                Text("You are a sapling! You've been out in the world making your environmentally friendly impact already, but now your impact is gonna skyrocket 🪾")
-                    .multilineTextAlignment(.center)
-                    .padding(20)
-                    .font(.custom("Poppins", size: 20))
-                    .foregroundColor(Color(red: 0.255, green: 0.53, blue: 0.35))
-                NavigationStack {
-                    /*@START_MENU_TOKEN@*/Text("Content")/*@END_MENU_TOKEN@*/
+                VStack {
+                    Text("You are a sapling! You've been out in the world making your environmentally friendly impact already, but now your impact is gonna skyrocket 🪾")
+                        .multilineTextAlignment(.center)
+                        .padding(20)
+                        .font(.custom("Poppins", size: 20))
+                        .foregroundColor(Color(red: 0.255, green: 0.53, blue: 0.35))
+                    NavigationStack {
+                        
+                        
+                    }
+                    
                 }
-                
             }
         }
     }
